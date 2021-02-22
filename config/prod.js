1d244d3c58f449973d570600ac98e63dc04a2860
lei.wang758 <lei.wang2@ucalgary.ca>
2021-02-22 15:55:57 -0700

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+// prod.js production keys are here
+
+module.exports = {
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY,
+	//mongodb+srv://leiwangProd:HSwY0ZuvsZ6buPXB@cluster0.6pzea.mongodb.net/prod?retryWrites=true&w=majority
+	// facebook, etc.
+}; //
