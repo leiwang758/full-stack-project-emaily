cb9fcd6e2e6e938bcc9c7b23957973aef02a4a8d
lei wang <lei.wang2@ucalgary.ca>
2021-02-24 15:55:04 -0700

    ictbranch

---
+++ client/src/setupProxy.js
@@ -0,0 +1,9 @@
+const createProxyMiddleware  = require("http-proxy-middleware");
+module.exports = function (app) {
+	app.use(
+		["/api", "/auth/google"],
+		createProxyMiddleware({
+			target: "http://localhost:5000", // CORS request / no cookies -> try to do sth malicious, same domin so not a issue.
+		})
+	);
+};
--- services/passport.js
+++ services/passport.js
@@ -10,10 +10,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-	User.findById(id)
-		.then(user => {
-			done(null, user);
-		})
+	User.findById(id).then((user) => {
+		done(null, user);
+	});
 });
 
 // client id: 643180449509-kkm3egn59jqqm0ac82s37ta8eb5g2j.apps.googleusercontent.com
@@ -21,34 +20,32 @@ passport.deserializeUser((id, done) => {
 // clientID: Public token - we can share this with the public
 // clientSecret: Private token - we dont't want anyone to see this !
 passport.use(
-	new GoogleStrategy(// any other auth, e.g. Facebook, Github
+	new GoogleStrategy( // any other auth, e.g. Facebook, Github
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: "/auth/google/callback",  //relative URL is issue number one that cause https -> http
+			callbackURL: "/auth/google/callback", //relative URL is issue number one that cause https -> http
 			// https -> http after proxy, solution 1: full path
-			proxy: true // solution 2
+			proxy: true, // solution 2
 		},
-		(accessToken, refreshToken, profile, done) => {
+		async (accessToken, refreshToken, profile, done) => {
 			// initiate a query
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
-				if (existingUser) {
-					// we already have a record with the given profile ID
-					done(null, existingUser); 
-					// Tell passport that we have finished creating a user and that it should
-					// now resume the auth process
-				} else {
-					// we don't have a user record with this ID, make a new record
-					new User({googleId: profile.id })
-					.save()
-					.then(user => done(null, user));
 
-				}
-			});
+			const existingUser = await User.findOne({ googleId: profile.id });
 
-			console.log("access token", accessToken);
-			console.log("refresh token", refreshToken);
-			console.log("profile: ", profile);
+			if (existingUser) {
+				// we already have a record with the given profile ID
+				return done(null, existingUser);
+				// Tell passport that we have finished creating a user and that it should
+				// now resume the auth process
+			}
+			// we don't have a user record with this ID, make a new record
+			const user = await new User({ googleId: profile.id }).save();
+			done(null, user);
+
+			// console.log("access token", accessToken);
+			// console.log("refresh token", refreshToken);
+			// console.log("profile: ", profile);
 		} // everything is hanging because google already authed
 	)
 ); // generic resgistration, passport be aware of new strategy
