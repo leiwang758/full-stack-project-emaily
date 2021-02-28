9dd21e888f09e77f25a504e8e8bc31291ab136ba
lei wang <lei.wang2@ucalgary.ca>
2021-02-27 20:12:16 -0700

    lab changes

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,15 +1,13 @@
+// this is action creator
 import axios from "axios"; //make ajax request
 import { FETCH_USER } from "./types";
 
-export const fetchUser = () => {
-	return function (dispatch) {
-		axios
-			.get("/api/current_user")
-			.then((res) => dispatch({ type: FETCH_USER, payload: res }));
-	};
-
-	// return {
-	// 	type:FETCH_USER,
-	// 	payload:request
-	// };
+export const fetchUser = () => async (dispatch) => {
+	const res = await axios.get("/api/current_user");
+	dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+// return {
+// 	type:FETCH_USER,
+// 	payload:request
+// };
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,10 @@
-export default function (state = {}, action) {
-	console.log(action);
+import { FETCH_USER } from "../actions/types";
+
+export default function (state = null, action) {
+	//console.log(action);
 	switch (action.type) {
+		case FETCH_USER: // we don't want this return to take too long
+			return action.payload || false;
 		default:
 			console.log(state);
 			return state;
